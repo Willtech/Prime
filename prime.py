841a47b1c8cc9c02832ae3d28039be88112c4f03
Damian Williamson <willtech@live.com.au>
2023-12-31 01:17:21 +1100

    Initial commit
    Signed-off-by: Damian Williamson <willtech@live.com.au>

---
+++ prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+## Prime
+# Takes an input and return all primes up to input
+# prime.py script
+# Source Code produced by Willtech 2023
+# v0.1 hand coded by HRjJ
+
+## setup dependencies
+import sys
+
+
