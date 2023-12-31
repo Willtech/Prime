b7f619b66587e71a052a686d952d944ae56c7d16
Damian Williamson <willtech@live.com.au>
2023-12-31 16:20:05 +1100

    prime.py input handler
    Signed-off-by: Damian Williamson <willtech@live.com.au>

--- prime.py
+++ prime.py
@@ -5,13 +5,20 @@
 # Source Code produced by Willtech 2023
 # v0.1 hand coded by HRjJ
 
+#prime.py [number to check] []
+
 ## setup dependencies
 import sys
 
 try:
  if sys.argv[1] is not None:
-  x = int(sys.argv[1])
+  if sys.argv[1] == "firstrun":
+   x = -1
+  else:
+   x = int(sys.argv[1])
 except:
   x = 531137992816767098689588206552468627329593117727031923199444138200403559860852242739162502265229285668889329486246501015346579337652707239409519978766587351943831270835393219031728127
+
+
 
 print(x)
