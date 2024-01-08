0acca6bd0e2eece338e21672bdf8c176943336b3
Damian Williamson <willtech@live.com.au>
2024-01-09 01:58:37 +1100

    primes.py working on mathematics logic
    Signed-off-by: Damian Williamson <willtech@live.com.au>

--- prime.py
+++ prime.py
@@ -7,13 +7,16 @@
 
 #prime.py [number to check] []
 
+print("Initialise...")
 ## setup dependencies
 import sys
 import re
 
-import mpmath
+from mpmath import *
+mp.dps = 5000000000 
 #@manual{mpmath,
 #  key     = {mpmath},
+
 #  author  = {The mpmath development team},
 #  title   = {mpmath: a {P}ython library for arbitrary-precision floating-point arithmetic (version 1.3.0)},
 #  note    = {{\tt http://mpmath.org/}},
@@ -26,11 +29,13 @@ try:
   if sys.argv[1] == "firstrun":
    x = -1
   else:
-   x = int(sys.argv[1])
+   mp.dps = len(str(int(sys.argv[1])))
+   x = mpf(sys.argv[1])
    if x < 1:
     x = x * -1
 except:
-  x = 531137992816767098689588206552468627329593117727031923199444138200403559860852242739162502265229285668889329486246501015346579337652707239409519978766587351943831270835393219031728127
+  mp.dps = 185
+  x = mpf('531137992816767098689588206552468627329593117727031923199444138200403559860852242739162502265229285668889329486246501015346579337652707239409519978766587351943831270835393219031728127')
 if x == 0:
  exit('0 will not be prime');
 
@@ -48,10 +53,19 @@ def filesave(data):
  primefile.close
 
 def findprime(x, i):
- if instance(x / i, int):
-  return(true)
+ mp.dps = len(str(x))
+ #print (str(mpf(x)), str(mpf(i))) #
+ #print (str(mpf(x) / i)) #
+ #print (str(mpf(x) / i).split(".")[1]) #
+ if str(mpf(x) / i).split(".")[1] == "0":
+  print (i, 'Doh!') #
+  return(False)
  else:
-  return(false)
+  print (i, 'Ding!') #
+  return(True)
+
+def createint(i):
+ return(int(i))
 
 #Development proof
 primes = 1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71
@@ -60,14 +74,48 @@ filesave(primes)
 primes = ""
 primes = fileread(primes)
 
-print (x)
-print (primes)
-print (primes[11])
-print (int(primes[11]))
+#print (x) #
+#print (primes) #
+#print (len(primes)) #
+#print (primes[11]) #
+#print (createint(primes[11])) #
+#print (len(str(x))) #
 
 #Runtime
 if x == -1: #firstrun
+ print ("Firstrun: Building _data")
  arp = 1
- while len(primes) <= 10000:
-  i = primes
+ primes = ""
+ primes = 1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71
+ while len(primes) <= 1000:
+  mp.dps = mpf(str(primes[len(primes)-1]))
+  x = mpf(str(primes[len(primes)-1]+2))
+  if findprime(x, 2):
+   i = primes[arp]
+   while i < x / i:
+    if findprime(x, i):
+     arp = arp + 1
+     i = primes[arp]
+    else:
+     arp = 1
+     i = primes[arp]
+     x = x + 2
+   print ("Found", x)
+   arp = 1
+   primes = (*primes, int(x))
+  else:
+   print (primes)
+   print ('Looking for', x)
+   exit ('Math error creating _data.');
 
+filesave(primes)
+fileread(primes)
+# print(primes) #
+print ('Completed creating data to', len(primes)-1, 'Primes');
+exit ('Largest Prime in Index:' + str(primes[len(primes)-1]))
+
+#sample
+mp.dps = len(str(primes[len(primes)-1]))
+x = mpf('169')
+i = 13
+print(str(mpf(x) / i))
