7e28e0250d513a07b591b822a1cb43f372e5096e
Damian Williamson <willtech@live.com.au>
2024-01-16 21:32:16 +1100

    prime.py Include not yet implementd statement when trying to resolve if n is prime. Use firstrun to generate up to 1,000,000th Prime.
    Signed-off-by: Damian Williamson <willtech@live.com.au>

--- prime.py
+++ prime.py
@@ -33,8 +33,9 @@ try:
    try:
     mp.dps = str(len(str(int(sys.argv[1]))))
     x = mpf(str(sys.argv[1]))
-   if x < 1:
-    x = x * -1
+   finally:
+    if x < 1:
+     x = x * -1
 except:
   mp.dps = 185
   x = mpf('531137992816767098689588206552468627329593117727031923199444138200403559860852242739162502265229285668889329486246501015346579337652707239409519978766587351943831270835393219031728127')
@@ -117,3 +118,4 @@ if x == -1: #firstrun
  exit ("--- %s Seconds ---" % (time.time() - tt))
 
 #Main
+exit('Not yet implement.')
